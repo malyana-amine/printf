d59bbff849ae0b62bef6007a0e774778846c84ed
mazzinno <abdennasr3@gmail.com>
2023-08-20 17:47:29 -0700

    new test for printf

--- main.h
+++ main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN
 #define MAIN
 
-
+#include <unistd.h>
 #include <stdio.h>
 #include <stdarg.h>
 
 void printChar(char c, int *count);
-void printString(char *str, int *count);
+void printStr(char *str, int *count);
 void printInt(int num, int *count);
 int _printf(const char *format, ...);
 
